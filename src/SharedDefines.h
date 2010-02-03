250769306d5f25a1342b14d0e07e3c1386ea45c6
Valroft <valroft@gmail.com>
2010-02-03 16:24:46 -0500

    Initial import of Bwee to GitHub.

---
+++ src/SharedDefines.h
@@ -0,0 +1,7 @@
+#ifndef _SHARED_DEFINES_H
+#define _SHARED_DEFINES_H
+
+#include "StdAfx.h"
+#define BWEE_CONFIGURATION_FILE "bwee.conf"
+
+#endif
---
+++ src/StdAfx.h
@@ -0,0 +1,23 @@
+#ifndef _STD_AFX_H
+#define _STD_AFX_H
+
+#include "MersenneTwister.h"
+#include "Common.h"
+#include "Log.h"
+#include "Config.h"
+#include "Thread.h"
+#include "SimpleSocket.h"
+
+#include "Field.h"
+#include "QueryResult.h"
+#include "MySQLConnection.h"
+
+
+
+#include "Replies.h"
+#include "IRCSession.h"
+#include "Singleton.h"
+
+#include "SharedDefines.h"
+
+#endif
